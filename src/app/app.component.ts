e1cb2215c62957fdb96cc62c3aeea389ecb7dc73
khoa2520 <86915350+khoa2520@users.noreply.github.com>
2022-05-15 17:24:19 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AdminPage_EPTTRAVEL_Web2FinalProject';
+}
