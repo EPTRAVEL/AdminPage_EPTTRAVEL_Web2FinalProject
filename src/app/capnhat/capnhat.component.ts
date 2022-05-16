ec943e5f6afcb3b4ebe6ecd2a754c89fb0b9dec0
thititing123 <93417411+thititing123@users.noreply.github.com>
2022-05-16 23:55:20 +0700

    update capnhat component

--- src/app/capnhat/capnhat.component.ts
+++ src/app/capnhat/capnhat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-capnhat',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CapnhatComponent implements OnInit {
 
-  constructor() { }
+  public regForm: FormGroup = this._formBuilder.group({
+    tentour : ['', [Validators.required, Validators.minLength(3)]],
+       matour : ['', Validators.required, Validators.minLength(7)],
+       diemden : ['', Validators.required],
+       confirmPass : ['']
+
+  }, )
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
