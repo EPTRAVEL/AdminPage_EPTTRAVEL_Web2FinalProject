ab1f4515193d32914442449abda62dc7bcf475a1
TLong1101 <longtt19406c@st.uel.edu.vn>
2022-05-31 09:26:54 +0700

    create and update thongbao

--- src/app/thongbao/thongbao.component.ts
+++ src/app/thongbao/thongbao.component.ts
@@ -6,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thongbao.component.css']
 })
 export class ThongbaoComponent implements OnInit {
+  public all: boolean = true;
+  public recent: boolean = false;
+  public unread: boolean = false;
+
+  selectAll() {
+    this.all = true;
+    this.recent = false;
+    this.unread = false;
+  }
+  selectRecent() {
+    this.all = false;
+    this.recent = false;
+    this.unread = true;
+  }
+  selectUnread() {
+    this.all = false;
+    this.recent = true;
+    this.unread = false;
+  }
 
   constructor() { }
 
